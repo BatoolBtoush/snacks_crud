775d9e531affac4f1475065892477eab84809dcf
BatoolBtoush <batoolbtoush98@gmail.com>
2022-05-16 15:17:11 +0300

    fixed styling

--- snacks_crud_project/snacks/models.py
+++ snacks_crud_project/snacks/models.py
@@ -9,10 +9,9 @@ class Snack(models.Model):
     title = models.CharField(max_length=64)
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
-    
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('snack_detail', kwargs={"pk": self.pk})
+        return reverse("snack_detail", kwargs={"pk": self.pk})
--- snacks_crud_project/snacks/urls.py
+++ snacks_crud_project/snacks/urls.py
@@ -9,9 +9,9 @@ from .views import (
 
 
 urlpatterns = [
-    path('', SnackListView.as_view(), name='snack_list'),
-    path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail'),
-    path('create/', SnackCreateView.as_view(), name='snack_create'),
-    path('<int:pk>/update/', SnackUpdateView.as_view(), name='snack_update'),
-    path('<int:pk>/delete/', SnackDeleteView.as_view(), name='snack_delete'),
-    ]
+    path("", SnackListView.as_view(), name="snack_list"),
+    path("<int:pk>/", SnackDetailView.as_view(), name="snack_detail"),
+    path("create/", SnackCreateView.as_view(), name="snack_create"),
+    path("<int:pk>/update/", SnackUpdateView.as_view(), name="snack_update"),
+    path("<int:pk>/delete/", SnackDeleteView.as_view(), name="snack_delete"),
+]
